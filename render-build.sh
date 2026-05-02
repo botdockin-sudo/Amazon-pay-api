b3210f29c0e1fd5aeb9fcc4ad1591ab61759285e
botdockin-sudo <botdock.in@gmail.com>
2026-05-03 01:31:23 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 npm install
-
-# Chrome install karne ke liye (Puppeteer ke liye zaroori hai)
-# Agar aap Render ke free tier par hain, toh niche wali lines kaam karengi
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
-  echo "Installing Chrome..."
-  # Ye command puppeteer ko chrome download karne par majboor karegi
-  npx puppeteer install
-fi
+# Isse Puppeteer apna browser khud install karega Render ke folder mein
+npx puppeteer install
